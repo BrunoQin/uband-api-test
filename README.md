469bc72add31f6a94f3af9917d5c48c0c2c31a1a
BrunoQin <brunoqin@163.com>
2018-04-19 16:14:46 +0800

    详细测试框架用法

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # uband-api-test
-### npm test your/test/script
+### npm test your/test/script（可不写全路径，只写脚本名）
--- __tests__/api_test/api.spec.js
+++ __tests__/api_test/api.spec.js
@@ -1,9 +1,36 @@
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
-describe('Posts', function () {
-  it('should be a teapot', function (done) {
+describe('Test', function () {
+
+  beforeAll(function () {
+    // Add our custom expect handler
+    frisby.addExpectHandler('isBeijing', function (res) {
+      expect(res.json.city).toBe("北京");
+    });
+  });
+
+  it('should response Beijing\'s weather', function (done) {
     frisby.get('http://www.sojson.com/open/api/weather/json.shtml?city=北京')
+      .expect('isBeijing')
       .expect('status', 200)
+      .expect('jsonTypes', {
+        message: Joi.string(),
+        pm25: Joi.number(),
+        pm10: Joi.number(),
+        shidu: Joi.string(),
+        quality: Joi.string()
+      })
+      .then(function (res) {
+        expect(res.json.message).toBe('Success !');
+        expect(res.json.data.pm25).toBe(150.0);
+      })
       .done(done);
   });
+
+  afterAll(function () {
+    // Remove said custom handler (if needed)
+    frisby.removeExpectHandler('isBeijing');
+  });
+
 });
